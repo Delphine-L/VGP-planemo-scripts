d42c14805367a9f7580321df2fe58609b23ece43
Delphine Lariviere <lariviere.delphine@gmail.com>
2025-08-18 11:14:59 -0400

    change hifi name fetch to prevent catching rdeval files

--- get_files_names.sh
+++ get_files_names.sh
@@ -2,7 +2,7 @@
 species_name=$1
 species_id=$2
 output=$3
-pacbios=($(aws --no-sign-request s3 ls genomeark/species/${species_name}/${species_id}/genomic_data/pacbio_hifi/ | grep fastq | awk '{print $4}'))
+pacbios=($(aws --no-sign-request s3 ls genomeark/species/${species_name}/${species_id}/genomic_data/pacbio_hifi/ | grep "fastq.gz$" | awk '{print $4}'))
 HiC_forward=($(aws --no-sign-request s3 ls genomeark/species/${species_name}/${species_id}/genomic_data/arima/ | grep R1 | awk '{print $4}'))
 HiC_reverse=($(aws --no-sign-request s3 ls genomeark/species/${species_name}/${species_id}/genomic_data/arima/ | grep R2 | awk '{print $4}'))
 bionano=($(aws --no-sign-request s3 ls genomeark/species/${species_name}/${species_id}/genomic_data/bionano/ | grep cmap | grep DLE | awk '{print $4}'))
